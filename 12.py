36066945603b63e4e0973f0b05627aebb2b26c8f
amirskm <amirmohiuddin@berkeley.edu>
2026-01-01 21:03:08 -0800

    Triangle problem

--- 12.py
+++ 12.py
@@ -1,17 +1,33 @@
-t = 0
+from math import isqrt
 
-while True:
-    y = 0
-    factors = 0 
-    t += 1
+triangle_num = 0
+#n = 20000
+n = 0
+factor_count = 0 
+# for natural_num in range(0, n + 1):
+#     triangle_num += natural_num
 
-    for n in range(1, t):
-        y += n
-    
-    for z in range(1, y + 1):
-        if y % z == 0:
-            factors += 1
-                
-    if factors > 501:
-        print(y)
-        break
+while factor_count < 500:
+    n += 1
+    factor_count = 0
+    triangle_num = n * (n + 1) // 2 
+    # print('triangle_num', triangle_num)
+
+    for factor in range(1, isqrt(triangle_num) + 12):
+        if triangle_num % factor == 0:
+        #  print('factor', factor)
+            factor_count += 2
+print('triangle_num', triangle_num)
+print('factor_count', factor_count)
+print('n', n)
+
+
+
+# n = 0 
+# factor_count = 6
+# triangle_num = 0
+# y = 0 
+
+# for x in range(0, 10):
+#     y += x
+# print(y)
