508abea9212f911540db119237b6b0883cf1b854
amirskm <amirmohiuddin@berkeley.edu>
2026-01-09 09:49:14 -0800

    functionizing p3

---
+++ 3optimized.py
@@ -0,0 +1,9 @@
+def sum_squared_natural_numbers(count):
+    n = 0
+    while n <= count:
+        total += n ** 2
+    return total
+
+# def get_sum_and_square_difference(count)
+
+print(sum_squared_natural_numbers(5))
