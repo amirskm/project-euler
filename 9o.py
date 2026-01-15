72997f83ec2ea14caa7e1f9a873f7ec484a6b1e5
amirskm <amirmohiuddin@berkeley.edu>
2026-01-15 10:02:01 -0800

    commit

--- 9o.py
+++ 9o.py
@@ -4,7 +4,7 @@ def product_of_triplet(triplet_sum):
     c = (b**2 + a**2)**.5
     
     for a in range(triplet_sum//3, 1, -1):
-        for b in range(triplet_sum//2, a + 1, -1):
+        for b in range((triplet_sum - a)//2, a + 1, -1):
             c = (b**2 + a**2)**.5
             if a < b < c and a + b + c == triplet_sum:
                 return(f"a is {a}, b is {b}, c is {c}, and product is {a * b * c}")
