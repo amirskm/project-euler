e11c2d548d9d4f05cdcbdec520a6a4e008d2fb36
amirskm <amirmohiuddin@berkeley.edu>
2026-01-22 22:13:53 -0800

    in progress

--- 15.py
+++ 15.py
@@ -1,18 +1,16 @@
-def count_routes_dp(n: int) -> int:
-    # Create a (n+1) x (n+1) grid filled with 0s
-    ways = [[0] * (n + 1) for _ in range(n + 1)]
+def get_lattice_path_count(rows, columns):
+    end_state = (columns + 1) * (rows + 1)
+    paths = 0
+    for i in range(0, end_state, 1):
+        print('i', i)
+    paths += 1
+    return paths
+    
 
-    # There's exactly 1 way to reach any cell on the top row (all Rights)
-    # and 1 way to reach any cell on the left column (all Downs)
-    for i in range(n + 1):
-        ways[0][i] = 1
-        ways[i][0] = 1
+print(get_lattice_path_count(1, 2))
+#  0 1 2 5
+#  0 1 4 5
+#  0 3 4 5
 
-    # Fill in the rest using the rule: from top + from left
-    for r in range(1, n + 1):
-        for c in range(1, n + 1):
-            ways[r][c] = ways[r - 1][c] + ways[r][c - 1]
-
-    return ways[n][n]
-
-print(count_routes_dp(2))
+#  + 1
+#  + 3
