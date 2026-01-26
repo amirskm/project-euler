48a97bc226eb9a2cc99551ae9deac1d6e7d98919
amirskm <amirmohiuddin@berkeley.edu>
2026-01-25 18:18:56 -0800

    woot

--- 92.py
+++ 92.py
@@ -1,10 +1,20 @@
 def get_square_digit(n):
-    # Convert to string to access individual digits
-    first = int(str(n)[0])
-    second = int(str(n)[1])
 
-    # Calculate the product (or square logic) and return it
-    result = first * second
-    return result
+    square_sum = 0
 
-print(get_square_digit(14))
+    for i in range(0, len(str(n))):
+        square_sum += int(str(n)[i])**2
+
+    if square_sum == 1 or square_sum == 89:
+        return square_sum
+    else:
+        return get_square_digit(square_sum)
+
+def get_count_below_limit(limit, destination):
+    count = 0
+    for j in range(1, limit + 1):
+        if get_square_digit(j) == destination:
+            count += 1
+    return count
+
+print(get_count_below_limit(9999999, 89))
