91f54959f5ccae96f0b57c6537b100393e16f92d
amirskm <amirmohiuddin@berkeley.edu>
2026-01-26 23:20:05 -0800

    woot

--- 18.py
+++ 18.py
@@ -1,13 +1,1 @@
-triangle_text = """3
-7 4
-2 4 6"""
-
-triangle = [[int(n) for n in line.split()] for line in triangle_text.splitlines()]
-
-total = 0
-
-for x in range(0, 3):
-    for y in range(0, len(triangle[x])):
-        if 
-        total += triangle[x][y]
-print(total)
+test 123
